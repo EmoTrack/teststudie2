3aa47363bcbb490b6fa887ec033385e8d3c804fd
EmoTrack <93148167+EmoTrack@users.noreply.github.com>
2021-10-26 17:34:30 +0200

    Update app.js

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('sw.js');
 }
 
+let params = new URLSearchParams(document.location.search.substring(1));
+let key = params.get("serial"); 
+
+localStorage.setItem('serial', key);
+
 let serial = localStorage.getItem('serial');
 
 let link1 = "www.example.de/?s=";
