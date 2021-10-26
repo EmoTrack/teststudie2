b9a597dc86c14ccc54e911e1904bc3e07d2a553c
EmoTrack <93148167+EmoTrack@users.noreply.github.com>
2021-10-26 17:37:10 +0200

    Update app.js

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('sw.js');
 }
 
-let params = new URLSearchParams(document.location.search.substring(1));
-let key = params.get("serial"); 
+//let params = new URLSearchParams(document.location.search.substring(1));
+//let key = params.get("serial"); 
 
-localStorage.setItem('serial', key);
+//localStorage.setItem('serial', key);
 
 let serial = localStorage.getItem('serial');
 
