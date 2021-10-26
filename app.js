08885714b7cb576d06d98007e43388b1b218e215
EmoTrack <93148167+EmoTrack@users.noreply.github.com>
2021-10-26 17:33:46 +0200

    Update app.js

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('sw.js');
 }
 
+let serial = localStorage.getItem('serial');
+
 let link1 = "www.example.de/?s=";
 
 let link2 = "www.example.de/?s=";
@@ -12,9 +14,8 @@ let link3 = "www.example.de/?s=";
 
 serial = String(serial);
 
-link1 = link1+serial;
-
-alert(link1);
-
+link1 = link1+serial;       
+     
+document.write("<p>Link: " + "EmoTrack".link(link1) + "</p>");
 
 
