91822d4fe35b7fbfa7a964cb792918cef8899f20
EmoTrack <93148167+EmoTrack@users.noreply.github.com>
2021-10-26 18:07:28 +0200

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('sw.js');
 }
 
-
 let params = new URLSearchParams(document.location.search.substring(1));
 let first = parseInt(params.get("first"), 10);
 
@@ -13,21 +12,29 @@ function fun() {
 let params = new URLSearchParams(document.location.search.substring(1));
 let key = params.get("serial"); 
 localStorage.setItem('serial', key);
-
-let serial = localStorage.getItem('serial');
-
-let link1 = "https://www.example.de/?s=";
-
-let link2 = "https://www.example.de/?s=";
-
-let link3 = "https://www.example.de/?s=";
-
-serial = String(serial);
-
-link1 = link1+serial;       
-     
-document.write("<p>Link: " + "EmoTrackfghj".link(link1) + "</p>");
-
 }
 
 if (first == 123) fun()
+
+let serial = localStorage.getItem('serial');
+serial = String(serial);
+
+let link1 = "https://www.soscisurvey.de/demotrack/?q=EMA_event&s=";
+
+let link2 = "https://www.soscisurvey.de/demotrack/?q=eat&s=";
+
+let link3 = "https://www.soscisurvey.de/demotrack/?q=emi&s=";
+
+link1 = link1+serial;
+link2 = link1+serial;
+link3 = link1+serial;   
+     
+document.write("<table>
+               <tr><td>"+"EmoTrack".link(link1)+"</td></tr>
+               <tr><td>"+"eatMotion".link(link2)+"</td></tr>
+               <tr><td>"+"EMI".link(link2)+"</td></tr>
+              </table> 
+    <div class="ios-prompt">
+    <p>To install this Web App in your iPhone/iPad press <img src="https://svgshare.com/i/AFm.svg" style="display: inline-block; margin-top: 4px; margin-bottom: -4px; height: 20px; width: auto;"> and then Add to Home Screen.
+    <img src="https://svgshare.com/i/AEL.svg" style="display: inline-block; margin-top: 4px; margin-bottom: -4px; height: 20px; width: auto;"></p>
+    </div>");
