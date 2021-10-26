95cad2aeb1140a6c8f288ad8a96ac8680cf79f0e
EmoTrack <93148167+EmoTrack@users.noreply.github.com>
2021-10-26 17:22:45 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -16,8 +16,6 @@ let params = new URLSearchParams(document.location.search.substring(1));
 let key = params.get("serial"); 
 
 localStorage.setItem('serial', key);
-
-let serial = localStorage.getItem('serial');
 });
 
 self.addEventListener('activate', (event) => {
