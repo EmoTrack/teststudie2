fa5d9f28d62501c6f0009b76e80ca1dec4dbdcd3
EmoTrack <93148167+EmoTrack@users.noreply.github.com>
2021-10-29 13:21:12 +0200

    Update app.js

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ link1 = link1+serial+txt;
 document.write("<table><tr><td>"+"EmoTrack".link(link1)+"</td></tr><tr><td>"+"eatMotion".link(link2)+"</td></tr><tr><td>"+"EMI".link(link3)+"</td></tr></table>");
   
 function myFunction() {
-  document.getElementById("ios.prompt").style.display = "block";
+  document.getElementById("ios-prompt").style.display = "block";
 }
 
 myFunction();
