9125b5f65b7f2b4199e3e2e79843da8cf17c3752
EmoTrack <93148167+EmoTrack@users.noreply.github.com>
2021-10-29 16:40:59 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,51 @@
-if('caches' in window) {
-        caches.open('cache_name').then((cache) => {
-            // ...
-        }).catch((err) => {
-            // ...
-        })
+const CACHE_NAME1 = "auth";
+const TOKEN_KEY1 = "token";
+const FAKE_TOKEN1 = "sRKWQu6hCJgR25lslcf5s12FFVau0ugi";
+
+const FAKE_ENDPOINT = "/fake-endpoint";
+
+const saveToken = async (token: string) => {
+  try {
+    const cache = await caches.open(CACHE_NAME);
+    const responseBody = JSON.stringify({
+      [TOKEN_KEY]: token
+    });
+    const response = new Response(responseBody);
+    await cache.put(FAKE_ENDPOINT, response);
+    console.log("Token saved! 🎉");
+  } catch (error) {
+    // It's up to you how you resolve the error
+    console.log("saveToken error:", { error });
+  }
+};
+
+
+const getToken = async () => {
+  try {
+    const cache = await caches.open(CACHE_NAME);
+    const response = await cache.match(FAKE_ENDPOINT);
+
+    if (!response) {
+      return null;
     }
 
+    const responseBody = await response.json();
+    return responseBody[TOKEN_KEY];
+  } catch (error) {
+    // Gotta catch 'em all
+    console.log("getToken error:", { error });
+  }
+};
+
+const displayCachedToken = async () => {
+  const cachedToken = await getToken();
+  console.log({ cachedToken });
+};
+
+// Uncomment the line below to save the fake token
+// saveToken(FAKE_TOKEN);
+
+displayCachedToken();
 
 /* Only register a service worker if it's supported */
 var isIOS = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
