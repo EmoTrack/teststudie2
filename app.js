670a1789ebd5ed101a99cfbb3d605a16152f3f2f
EmoTrack <93148167+EmoTrack@users.noreply.github.com>
2021-10-29 13:11:50 +0200

    Update app.js

--- app.js
+++ app.js
@@ -31,6 +31,10 @@ let link3 = "https://www.soscisurvey.de/demotrack/?q=emi";
 
 link1 = link1+serial+txt;
   
-     
 document.write("<table><tr><td>"+"EmoTrack".link(link1)+"</td></tr><tr><td>"+"eatMotion".link(link2)+"</td></tr><tr><td>"+"EMI".link(link3)+"</td></tr></table>");
   
+function myFunction() {
+  document.getElementById("ios.prompt").style.display = "block";
+}
+
+myFunction();
