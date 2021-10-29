88fb5ea3d65cc1c5d386dd7aa419cad3e1620bda
EmoTrack <93148167+EmoTrack@users.noreply.github.com>
2021-10-29 14:29:06 +0200

    Update app.js

--- app.js
+++ app.js
@@ -32,11 +32,24 @@ let link2 = "https://www.soscisurvey.de/demotrack/?q=eat";
 let link3 = "https://www.soscisurvey.de/demotrack/?q=emi";
 
 link1 = link1+serial+txt;
-  
-document.write("<table><tr><td>"+"EmoTrack".link(link1)+"</td></tr><tr><td>"+"eatMotion".link(link2)+"</td></tr><tr><td>"+"EMI".link(link3)+"</td></tr></table>");
-  
+    
 var isInStandaloneMode = ('standalone' in window.navigator) && (window.navigator.standalone);
 
 if (isIOS && (first == 123)) {
-  document.getElementById("ios-prompt").style.display = "block"; 
+document.getElementById("ios-prompt").style.display = "block";
+let params2 = new URLSearchParams(document.location.search.substring(1));
+let key1 = params2.get("serial");
+const SHARED_DATA_ENDPOINT = '/token';
+      function generateToken() {
+        fetch(SHARED_DATA_ENDPOINT, { method: "POST", body: JSON.stringify(serial;
+        });
 }
+
+if (isIOS && (first != 123)) {
+fetch(SHARED_DATA_ENDPOINT).then(response => response.json()).then(data => {
+     console.log('Got', data, 'from cache');
+     link1 = link1+response+txt;
+});
+}
+
+document.write("<table><tr><td>"+"EmoTrack".link(link1)+"</td></tr><tr><td>"+"eatMotion".link(link2)+"</td></tr><tr><td>"+"EMI".link(link3)+"</td></tr></table>");
