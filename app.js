734ef194fe4d67bf99865096df359d1f6eb49dea
EmoTrack <93148167+EmoTrack@users.noreply.github.com>
2021-11-27 15:44:25 +0100

    Update app.js

--- app.js
+++ app.js
@@ -21,6 +21,8 @@ if ((time1 - time0) > 15*60*100){
 window.location.href = 'token.html';}
 }
 
+sperre();
+
 function fun1() {
 let params1 = new URLSearchParams(document.location.search.substring(1));
 let key = params1.get("d"); 
