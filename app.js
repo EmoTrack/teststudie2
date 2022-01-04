cc2bd25255684133e9e2af07e330fba337dae679
EmoTrack <93148167+EmoTrack@users.noreply.github.com>
2022-01-05 00:01:58 +0100

    Update app.js

--- app.js
+++ app.js
@@ -10,19 +10,7 @@ if ('serviceWorker' in navigator) {
   });
 }
 
-var isIOS = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
-var isFirefox = /Mozilla/.test(navigator.userAgent);
-console.log(isIOS);
-console.log(isFirefox);
-
-let link = "https://www.soscisurvey.de/emotrack2/?q=emotrack&s=";
-
-function fun2() {
-let params = new URLSearchParams(document.location.search.substring(1));
-first = parseInt(params.get("first"), 10);
-}
-
-function fun3() {
+function refer() {
 let params1 = new URLSearchParams(document.location.search.substring(1));
 value = parseInt(params1.get("s"), 10);
 console.log(value);
@@ -33,42 +21,49 @@ let check_link1 = link2+value;
 window.location.href = check_link1;
 }
 }
+refer();
+
+var isIOS = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
+var isFirefox = /Mozilla/.test(navigator.userAgent);
+
+function install() {
+
+  let params = new URLSearchParams(document.location.search.substring(1));
+  first = parseInt(params.get("first"), 10);
+  
+  if (first == 123){
+    if (isIOS) {
+    document.getElementById("ios-prompt").style.display = "block";
+    document.getElementById("eingabefeld").style.display = "none";
+    }
+    if (isFirefox) {
+    document.getElementById("firefox-prompt").style.display = "block";
+    document.getElementById("eingabefeld").style.display = "none";
+    }
+    if (!isFirefox) {
+    document.getElementById("android-prompt").style.display = "block";
+    document.getElementById("eingabefeld").style.display = "none";
+    }
+  }
+
+  if (first == 666) {
+    localStorage.removeItem('serial');
+    document.getElementById("eingabefeld").style.display = "block";
+    alert("Fehlerhafter Code");
+  }
+}
+
+install();
 
 function send(){
-let token = document.querySelector("#token").value;
-console.log (token);
-localStorage.setItem('serial', token);
-check = String(token);
-let link1 = "https://www.soscisurvey.de/emotrack2/?q=emotrack01&s=";
-let check_link = link1+check;
-window.location.href = check_link;
+    let token = document.querySelector("#token").value;
+    localStorage.setItem('serial', token);
+    check = String(token);
+    let link1 = "https://www.soscisurvey.de/emotrack2/?q=emotrack01&s=";
+    let check_link = link1+check;
+    window.location.href = check_link;
 }
-
-fun3();
-fun2();
-console.log(first);
-
-if (isIOS && (first == 123)) {
-document.getElementById("ios-prompt").style.display = "block";
-document.getElementById("eingabefeld").style.display = "none";
-}
-
-if (isFirefox && (first == 123)) {
-document.getElementById("firefox-prompt").style.display = "block";
-document.getElementById("eingabefeld").style.display = "none";
-}
-
-if (!isFirefox && (first == 123)) {
-document.getElementById("android-prompt").style.display = "block";
-document.getElementById("eingabefeld").style.display = "none";
-}
-
-if (first == 666) {
-  localStorage.removeItem('serial');
-  document.getElementById("eingabefeld").style.display = "block";
-  alert("Fehlerhafter Code");
-}
-
+  
 let serial = localStorage.getItem('serial');
 
 if (serial == null){
